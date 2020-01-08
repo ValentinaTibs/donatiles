b2f408069d11d24bdd0aee33447d20c0592e2b42
Valentina Tibaldo <valentina.tibaldo@quixant.com>
2020-01-08 11:52:36 +0100

    changed names

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,30 +5,22 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
+import taleoftiles.views
 
 # To add a new path, first import the app:
 # import blog
-#
+
 # Then add the new path:
 # path('blog/', blog.urls, name="blog")
 #
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+    path("", taleoftiles.views.index, name="index"),
+    path("db/", taleoftiles.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG: # new
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-     
-# if settings.DEBUG is True:
-#     urlpatterns += static((r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}) )
-#     #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     # More paths/urls
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
