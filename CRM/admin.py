27b0a0a4ba3a8aee6bc483d0089923892558eb9f
Valentina <tibaldo@di.uniroma1.it>
2020-05-06 11:39:55 +0200

    .

--- CRM/admin.py
+++ CRM/admin.py
@@ -37,15 +37,15 @@ close_them.short_description = "Mark as Closed"
 class ChartAdmin(admin.ModelAdmin):
 	model = Chart
 
-	list_display = ('session_id','user','completion_status','is_sample','created_at','modified_at','_num_prods')
-	actions = [close_them]
-	readonly_fields  = ( 'session_id','is_sample','created_at','modified_at')
-	inlines = [ChartItemStackedAdmin,]
+	# list_display = ('session_id','user','completion_status','is_sample','created_at','modified_at','_num_prods')
+	# actions = [close_them]
+	# readonly_fields  = ( 'session_id','is_sample','created_at','modified_at')
+	# inlines = [ChartItemStackedAdmin,]
 
-	def _num_prods(self, obj):
-		return obj.num_prods()
-	_num_prods.short_description = "Number of Products"
-	_num_prods.admin_order_field = 'num_prods'
+	# def _num_prods(self, obj):
+	# 	return obj.num_prods()
+	# _num_prods.short_description = "Number of Products"
+	# _num_prods.admin_order_field = 'num_prods'
 
 admin.site.register(Profile,ProfileAdmin)
 admin.site.register(Chart,ChartAdmin)
