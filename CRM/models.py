f44c93c65975eff8bf9d6282e0f100aa65276228
Valentina <tibaldo@di.uniroma1.it>
2020-05-11 16:27:05 +0200

    .

--- CRM/models.py
+++ CRM/models.py
@@ -35,7 +35,9 @@ class Profile(models.Model):
     location    = models.CharField(max_length=30, blank=True)
     birth_date  = models.DateField(null=True, blank=True)
 
-
+    def __str__(self):
+        return self.user.username        
+   
 class Shipping(models.Model):
     #2do this must become a one to Many because we want to keep track of old shippings
     user                = models.OneToOneField(Profile, on_delete=models.CASCADE, null = True)
