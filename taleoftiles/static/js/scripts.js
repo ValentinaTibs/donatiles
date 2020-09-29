2a08a984df2d57bf76a5454050fa8e6bda2868e7
Valentina <tibaldo.valentina@gmail.com>
2020-09-29 19:02:14 +0200

    trying tounderstand misteries

--- taleoftiles/static/js/scripts.js
+++ taleoftiles/static/js/scripts.js
@@ -57,22 +57,8 @@
     s = e.length,
     a = document.getElementById("gallerybtn-prev"),
     d = document.getElementById("gallerybtn-next"),
-    // [].forEach.call(e, function(e, t) {
-    //     e.dataset.id = t;
-    //     var n = document.createElement("li");
-    //     l.appendChild(n),
-    //     n.addEventListener("click", function() {
-    //         o(t)
-    //     })
-    // }),
-    // n = l.querySelectorAll("li"),
-    // d.addEventListener("click", function() {
-    //     s - 1 <= r || o(+e[++r].dataset.id)
-    // }),
-    // a.addEventListener("click", function() {
-    //     r <= 0 || o(+e[--r].dataset.id)
-    // }),
-    // o(0),
+
+    
     document.querySelectorAll("[data-overlay]").forEach(function(e) {
         e.addEventListener("click", function(e) {
             t(e, document.getElementById("overlay" + this.dataset.overlay))
@@ -96,3 +82,5 @@
         }, 3e3)
     })
 }(window);
+
+
