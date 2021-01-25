71c5d5f2f24ea162933002ddb873621ca6471f67
Valentina <tibaldo.valentina@gmail.com>
2021-01-25 14:38:29 +0100

    .

--- gettingstarted/deploy_settings.py
+++ gettingstarted/deploy_settings.py
@@ -20,7 +20,7 @@ from django.utils.translation import ugettext_lazy as _
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
