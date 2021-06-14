8d34fad2df45fea09f1f014217cab3670e939c0f
Valentina <tibaldo.valentina@gmail.com>
2021-06-14 11:26:13 +0200

    .

--- layout/models.py
+++ layout/models.py
@@ -181,8 +181,6 @@ class MailTemplate(models.Model):
             logger.error(e.body)
 
     def send_wallpaper_req(self,request, email, width, height, notes, name_surname,  telephone):
-        print( " --->")
-        print( email, width, height, notes, name_surname,  telephone)
         cLng = translation.get_language()
         email_template_name = "email/"+cLng+"/wallpaper_email.txt"
         c = {                
