61206ea7d87f08f5928c18fc5dcacbece23f1a9f
Valentina <tibaldo.valentina@gmail.com>
2021-10-14 14:32:14 +0200

    rimossi quadrati

--- taleoftiles/views.py
+++ taleoftiles/views.py
@@ -29,7 +29,6 @@ def index(request):
 
     return render(request, "home.html",{
         'layout_elems'  : home_elems,
-        'tags'          : home_tags,
         'secondary_post': secondary_post,
         'main_post'     : main_post,
         'home_prods'    : home_prods
